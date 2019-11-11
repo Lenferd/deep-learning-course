d57488cf84a2fd08872a7ef4b5755159487a3d9d
Lenferd <DeysonSH@gmail.com>
2019-11-12 00:21:50 +0300

    Play with numpy

--- Lab 1/tests.py
+++ Lab 1/tests.py
@@ -18,12 +18,22 @@ import numpy as np
 #         with self.assertRaises(TypeError):
 #             s.split(2)
 
-
 class TestNumpy(unittest.TestCase):
-
     def test_generate_array(self):
-        np.array([2, 2], float)
-        self.assertTrue(True)
+        arr = np.array([2, 2], float)
+        print("test_generate_array\n {}".format(arr))
+
+    def test_matrix_not_rec_creation(self):
+        matrix = np.matrix([[1, 2], [3, 4]])
+        print("test_matrix_not_rec_creation\n {}".format(matrix))
+
+    def test_matrix_rec_creation(self):
+        matrix = np.array([[1, 2], [3, 4]])
+        print("test_matrix_rec_creation\n {}".format(matrix))
+
+    def test_transpose(self):
+        matrix = np.array([[1, 2], [3, 4]])
+        print("test_transpose\n {} \n {}".format(matrix, matrix.T))
 
 class TestSigmoid(unittest.TestCase):
 
