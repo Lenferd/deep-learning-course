06853a096d73bbc0b6b62bcf8027b339343e17e9
Lenferd <DeysonSH@gmail.com>
2019-11-12 00:42:01 +0300

    Continue play with np arrays

--- Lab 1/tests.py
+++ Lab 1/tests.py
@@ -1,23 +1,6 @@
 import unittest
 import numpy as np
 
-
-# class TestStringMethods(unittest.TestCase):
-#
-#     def test_upper(self):
-#         self.assertEqual('foo'.upper(), 'FOO')
-#
-#     def test_isupper(self):
-#         self.assertTrue('FOO'.isupper())
-#         self.assertFalse('Foo'.isupper())
-#
-#     def test_split(self):
-#         s = 'hello world'
-#         self.assertEqual(s.split(), ['hello', 'world'])
-#         # check that s.split fails when the separator is not a string
-#         with self.assertRaises(TypeError):
-#             s.split(2)
-
 class TestNumpy(unittest.TestCase):
     def test_generate_array(self):
         arr = np.array([2, 2], float)
@@ -35,11 +18,32 @@ class TestNumpy(unittest.TestCase):
         matrix = np.array([[1, 2], [3, 4]])
         print("test_transpose\n {} \n {}".format(matrix, matrix.T))
 
-class TestSigmoid(unittest.TestCase):
+    def test_permutation(self):
+        number = 10
+        # Permutation for numbers from 0 to 10
+        permutation = np.random.permutation(number)
+        print("test_permutation\n {} \n {}".format(number, permutation))
 
-    def test_correct_calculating(self):
-        x = 2.3
-        
+    def test_random_array_float(self):
+        arr = np.random.rand(2)
+        print("test_random_array_float\n {}".format(arr))
+
+    def test_random_array_int(self):
+        max = 10
+        size = 2
+        arr = np.random.randint(max, size=size)
+        print("test_random_array_int\n {}".format(arr))
+
+    def test_shuffle_array(self):
+        size = 10
+        arr = np.random.randint(10, size=size)
+        permutation = np.random.permutation(size)
+        # This is for ndarray.
+        # Because there we have 2 dimensional array, and we shuffle only second one (columns for example)
+        # shuffled_arr = arr[:, permutation]
+        shuffled_arr = arr[permutation]
+        print("test_shuffle_array\n {} \n {}".format(arr, shuffled_arr))
+
 
 if __name__ == '__main__':
     unittest.main()
