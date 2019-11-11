aa99d6bb647df4b71ed0176f870666d2e3cf66e9
Lenferd <DeysonSH@gmail.com>
2019-11-12 00:42:14 +0300

    shuffled data

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -7,6 +7,20 @@ from helpers.viewer import plot_digit2
 if __name__ == '__main__':
     X, y = get_mlxtend_mnist()
 
-    X_train = X[:].T
-    plot_digit2(X_train[:, 3])
+    # Use only label 0 and 0 or 1 as result
+    y_new = np.zeros(y.shape)
+    y_new[np.where(y == 0.0)[0]] = 1
+    y = y_new
 
+    # Divide data to train and test
+    size_train = 60000
+    size_test = X.shape[0] - size_train    # amount of rows
+
+    X_train, X_test = X[:size_train].T, X[size_train:].T    # (5000, 784 (28x28)
+    y_train, y_test = y[:size_train].T, y[size_train:].T    # (5000,), reshape can be used instead
+
+    # Shuffle training set (why?)
+    np.random.seed(42)
+    shuffled_indexes = np.random.permutation(size_train)
+
+    X_train, y_train = X_train[:, shuffled_indexes], y_train[:, shuffled_indexes]
