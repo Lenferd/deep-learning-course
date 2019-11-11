d337f77f9dc3b9d2d1b439f44b5e22ce0f511615
Lenferd <DeysonSH@gmail.com>
2019-11-12 02:16:07 +0300

    Fix sigmoid function

--- Lab 1/activations.py
+++ Lab 1/activations.py
@@ -1,11 +1,12 @@
-import math
 import numpy as np
 
 
 class Sigmoid:
+    @staticmethod
     def calculate(x):
-        return 1 / (1 + math.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
+    @staticmethod
     def derivative(x):
         return Sigmoid.calculate(x) * (1 - Sigmoid.calculate(x))
 
